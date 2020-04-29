7f5842bf91ec1cc5236111d0868949f5dd27cce6
Ilmari <eoksanen@gmail.com>
2020-04-29 19:30:50 +0300

    45

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,38 @@
 const dummy = (blogs) => {
-    // ...
+    
+  return 1
   }
+
+  const totalLikes = (blogs) => {
+
+    const result = blogs.reduce((sum, bl)=>{
+
+  
+
+    return sum = sum + bl.likes
+
+ 
+  },0)
+  console.log(result)
+return result
+  }
+  const maxLikes = (blogs) => {
+
+
+    const likes = blogs.map(blog => {
+    
+      return blog.likes
+
+    })
+    console.log(Math.max(...likes))
+    return Math.max(...likes)
+
+
+  }
+
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    maxLikes,
   }
