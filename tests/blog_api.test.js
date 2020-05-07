5456fa611eb467c1409485cf098052b51eefdd2b
Ilmari <eoksanen@gmail.com>
2020-05-07 12:22:01 +0300

    49

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,3 +1,4 @@
+const helper = require('./test_helper')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -9,6 +10,21 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('is id defined', async () => {
+
+  const blogs = await helper.blogsInDb()
+  //const blogs = await Blog.find({})
+  /*
+  api
+  .get('/api/blogs')
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+  //.expect(blogs.id).toBeDefined()
+  */
+ expect(blogs[0].id).toBeDefined()
+  console.log(blogs)
 })
 
 afterAll(() => {
