521cb80de4ff738bcd8f51cd769f43e73a57fe5e
Ilmari <eoksanen@gmail.com>
2020-05-12 18:04:38 +0300

    414

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,22 +2,46 @@ const blogsRouter = require('express').Router()
 
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs.map(blog => blog.toJSON()))
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
+})
+  
+  blogsRouter.post('/', async (request, response) => {
+
+    const body = request.body
+    if(body.likes === undefined)
+    {body.likes = 0}
+    if(body.title !== undefined && body.url !== undefined){
+
+      console.log(body)
+      const newBlog = new Blog(body)
+      const savedBlog = await newBlog.save()
+      response.status(200).json(savedBlog.toJSON)
+    }
+    else {response.status(400).end()}
+  })
+
+  blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  })
+
+  blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+
+    console.log(body)
+  
+    const blog = {
+      likes: body.likes,
+    }
+  
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      .then(updatedBlog => {
+        response.json(updatedBlog.toJSON())
       })
+      .catch(error => next(error))
   })
   
-  blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
 
   module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,6 +5,74 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log('all deleted')
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+/*
+   helper.initialBlogs.forEach(async (blog) => {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+    
+    console.log('saved')
+   })
+   */
+  console.log('done')
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = helper.initialBlogs[2]
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(n => n.title)
+
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
+  )
+
+  const titles = blogsAtEnd.map(r => r.title)
+
+  expect(titles).not.toContain(blogToDelete.title)
+})
+
+test('blog can be edited', async () => {
+  const allBlogs = await helper.blogsInDb()
+
+
+console.log(allBlogs[0].id)
+  await api
+    .put(`/api/blogs/${allBlogs[0].id}`)
+    .send({ likes: 100 })
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -15,37 +83,64 @@ test('blogs are returned as json', async () => {
 test('is id defined', async () => {
 
   const blogs = await helper.blogsInDb()
-  //const blogs = await Blog.find({})
-  /*
-  api
-  .get('/api/blogs')
+  expect(blogs[0].id).toBeDefined()
+ // console.log(blogs)
+})
+
+test('blogs without likes goes to zero', async () => {
+  const testBlog = {
+    title: "test",
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+  }
+  console.log(testBlog)
+
+  await api
+  .post('/api/blogs')
+  .send(testBlog)
   .expect(200)
   .expect('Content-Type', /application\/json/)
-  //.expect(blogs.id).toBeDefined()
-  */
- expect(blogs[0].id).toBeDefined()
-  console.log(blogs)
+
+const blogsAtEnd = await helper.blogsInDb()
+
+expect(blogsAtEnd[blogsAtEnd.length-1].likes).toBeDefined()
+
 })
 
-test('add blog test', async () => {
+describe('if added blog not contain title and url server res error code 400 bad req', () => {
 
+test('title missing', async () => {
 
-  api.post('/api/blogs', async (request, Response) => {
-    const body = request.body
+  const testBlog = {
+   // title: "url missing",
+    author: 'Robert C. Martin',
+    url: "www.www.www"
+  
+  }
 
-    const newBlog = new Blog({
-      content: 'testBlog',
-      title: 'Testing',
-      author: 'String',
-      url: 'String',
-      likes: 100
-    })
-    
-  const savedBlog = await newBlog.save()
-  response.json(savedBlog.toJSON())
+  await api
+  .post('/api/blogs')
+  .send(testBlog)
+  .expect(400)
+
   })
-})
 
+  test('url missing', async () => {
+
+    const testBlog2 = {
+      title: "url missing",
+      author: 'Robert C. Martin',
+     // url: "www.www.www"
+    
+    }
+  
+    await api
+    .post('/api/blogs')
+    .send(testBlog2)
+    .expect(400)
+  
+    })
+})
 
 afterAll(() => {
   mongoose.connection.close()
