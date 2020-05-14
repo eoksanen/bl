454f71889b22d1a75d2efef0e79188f00dda8d5d
Ilmari <eoksanen@gmail.com>
2020-05-14 19:46:57 +0300

    420

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -26,7 +28,14 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
 
   app.use(cors())
   app.use(express.json())
+  app.use(middleware.tokenExtrator)
   app.use('/api/blogs', blogsRouter)
+  app.use('/api/users', usersRouter)
+  app.use('/api/login', loginRouter)
+
+  app.use(middleware.unknownEndpoint)
+  app.use(middleware.errorHandler)
+
   app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
   module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,41 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
-  
+
+ 
   blogsRouter.post('/', async (request, response) => {
 
     const body = request.body
+    console.log('printed TOKEN: ',request.token)
+    const decodedToken = jwt.verify(request.token.substring(7), process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     if(body.likes === undefined)
     {body.likes = 0}
     if(body.title !== undefined && body.url !== undefined){
 
       console.log(body)
-      const newBlog = new Blog(body)
+      const newBlog = new Blog(
+        {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+        })
       const savedBlog = await newBlog.save()
+      user.notes = user.blogs.concat(savedBlog._id)
+      await user.save()
       response.status(200).json(savedBlog.toJSON)
     }
     else {response.status(400).end()}
@@ -33,7 +52,7 @@ blogsRouter.get('/', async (request, response) => {
     console.log(body)
   
     const blog = {
-      likes: body.likes,
+      likes: body.likes
     }
   
     Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
